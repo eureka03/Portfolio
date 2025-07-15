b63569568d3c678db162ee756b8593d11fd297f4
Eureka Baloyi <eurekaripfumelo@gmail.com>
2025-07-15 12:17:02 +0200

    changes added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,8 @@
 import {Link} from 'react-router-dom';
 import './Navbar.css';
+import { FaGithub,FaLinkedinIn  } from "react-icons/fa";
+
+
 export default function Navbar(){
     return(
         <nav className="">
@@ -10,6 +13,9 @@ export default function Navbar(){
                 <div className='nav-links'>
                     <Link to='./About.js'>About</Link>
                     <Link to='./Projects.js'>Portfolio</Link>
+                    <Link to='https://github.com/eureka03'><FaGithub/></Link>
+                    <Link to='https://www.linkedin.com/in/eureka-baloyi-190a74203/'><FaLinkedinIn /></Link>
+                    <button>Hire me </button>
                 </div>
             </div>
         </nav>
