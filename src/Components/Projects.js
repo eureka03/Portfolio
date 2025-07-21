8a385a29e54bc5df4be045e4be18c051868b7572
Eureka Baloyi <eurekaripfumelo@gmail.com>
2025-07-21 11:45:43 +0200

    projects changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,23 @@
+import './Projects.css';
+
 export default function Projects(){
     return(
         <>
+            <div className="projects-container">
+                
+                <div className='header'>
+                    <div><hr/></div>
+                    <div className='content'>Some of my latest work</div>
+                    <div><hr/></div>
+                    
+                </div>
+                
+                <div className='projects'>
+                    <div className='project'>proj1</div>
+                    <div className='project'>proj2</div>
+                    <div className='project'>proj3</div>
+                </div>
+            </div>
         
         </>
     )
