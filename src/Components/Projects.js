0eeabbad37d5d5c6875d2853e7545519b75df5f1
Eureka Baloyi <eurekaripfumelo@gmail.com>
2025-07-21 12:19:31 +0200

    changes added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,7 @@
 import './Projects.css';
+import Kota from '../Assets/Kota.png';
+import BookingSystem from '../Assets/Bookingsystem.png';
+import {Link} from 'react-router-dom';
 
 export default function Projects(){
     return(
@@ -13,9 +16,24 @@ export default function Projects(){
                 </div>
                 
                 <div className='projects'>
-                    <div className='project'>proj1</div>
-                    <div className='project'>proj2</div>
-                    <div className='project'>proj3</div>
+                    <div className='project'>
+                        <img src={Kota} alt='project1'/>
+                        <h3>Kota Business</h3>
+                        <p>Side project</p>
+                        <Link to=''>Check out Kota Business</Link>
+                    </div>
+                    <div className='project'>
+                       <img src='' alt='project2'/>
+                        <h3>Trip</h3>
+                        <p>Side project</p>
+                        <Link to=''>Check out Trip</Link>
+                    </div>
+                    <div className='project'>
+                        <img src={BookingSystem} alt='project3'/>
+                        <h3>Salon booking system</h3>
+                        <p>Side project</p>
+                        <Link to=''>Check out Salon Booking System</Link>
+                    </div>
                 </div>
             </div>
         
