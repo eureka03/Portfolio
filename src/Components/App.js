6fa714c31e75393199bdcfc504f9fe20a5e70b10
Eureka Baloyi <eurekaripfumelo@gmail.com>
2025-07-24 10:37:49 +0200

    changes added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 
 
 import Navbar from './Navbar';
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import {HashRouter,Routes,Route} from 'react-router-dom';
 import Home from './Homepage';
 import About from './About';
 import Projects from './Projects';
@@ -9,14 +9,14 @@ import Projects from './Projects';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/About' element={<About/>}/>
           <Route path='/Projects' element={<Projects/>}/>
         </Routes>
-      </Router>
+      </HashRouter>
       
     </div>
   );
